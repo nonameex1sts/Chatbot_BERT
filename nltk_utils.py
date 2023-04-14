1437515ec0ae6741755bd98a396de1aef3f00ea2
hieu <mhieu.vn@gmail.com>
2023-04-14 14:12:28 +0700

    ok

--- nltk_utils.py
+++ nltk_utils.py
@@ -1,8 +1,12 @@
 import numpy as np
 import nltk
-nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
+
+nltk.download('punkt')
+
+
 stemmer = PorterStemmer()
+
 
 def tokenize(sentence):
     """
@@ -37,7 +41,7 @@ def bag_of_words(tokenized_sentence, words):
     # initialize bag with 0 for each word
     bag = np.zeros(len(words), dtype=np.float32)
     for idx, w in enumerate(words):
-        if w in sentence_words: 
+        if w in sentence_words:
             bag[idx] = 1
 
     return bag
